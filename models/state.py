e7c0dcf043e2019f4066dd7e906db1254a3836a5
I7RANK <franfgh2000@hotmail.com>
2021-01-20 01:35:50 +0000

    Adding if and the output is diferent

--- models/state.py
+++ models/state.py
@@ -2,30 +2,32 @@
 """ State Module for HBNB project """
 
 
+import models
 from models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+import os
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    # Check engine
-    cities = relationship(
-        "City",
-        backref="states",
-        cascade="all, delete, delete-orphan"
-    )
-
-    @property
-    def cities_method(self):
-        """ Getter cities """
-        list_city = []
-        for city in list(models.storage.all(City)):
-            if self.id == city.state_id:
-                list_city.append(City)
-        return list_city
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            "City",
+            backref="states",
+            cascade="all, delete, delete-orphan"
+        )
+    else:
+        @property
+        def cities(self):
+            """ Getter cities """
+            list_city = []
+            for city in models.storage.all(City).values():
+                if self.id == city.state_id:
+                    list_city.append(City)
+            return list_city
