f3f8a173193f5a4b8a3c397039b775b429368003
I7RANK <franfgh2000@hotmail.com>
2021-01-20 18:25:56 +0000

    Uploading task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" starts a Flask web application with 7 routes and templates
+"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+data = storage.all(State).values()
+
+
+@app.route('/states_list', strict_slashes=False)
+def render_states_list():
+    """returns a web page with a states list
+
+    Returns:
+        web_page: states list
+    """
+    return render_template("7-states_list.html", data=data)
+
+
+# It's executed every time the application context tears down
+@app.teardown_appcontext
+def bye():
+    """ Close the SQLAlquemy session """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="5000")
