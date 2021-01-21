722f834ee4cde72509f6a431fe1eb0330717fd26
I7RANK <franfgh2000@hotmail.com>
2021-01-21 03:19:17 +0000

    Uploading task 11 no complete

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" starts a Flask web application with 7 routes and templates
+"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+
+# It's executed every time the application context tears down
+@app.teardown_appcontext
+def bye(error):
+    """ Close the SQLAlquemy session """
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def render_cities_by_states():
+    """returns a web page with a cities_by_states list
+
+    Returns:
+        web_page: states list
+    """
+    st = storage.all(State).values()
+    ci = storage.all(City).values()
+    return render_template("10-hbnb_filters.html", states=st, cities=ci)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="5000")
